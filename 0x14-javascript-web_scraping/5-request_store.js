a636d382d0c77cd4f706b331be93752160ef3d11
girlbehindthescreen <igwuokuchristabel@gmail.com>
2023-04-25 05:59:40 -0700

    task

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
+equest(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
